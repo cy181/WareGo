f434685fdb72934030d5562b135c77d148341a98
harrisoncheung <calecheung@gmail.com>
2026-07-09 17:09:32 +0900

    added sort menu, might need to change some css to make it look better

--- resources/views/layout/category-layout.blade.php
+++ resources/views/layout/category-layout.blade.php
@@ -62,6 +62,17 @@
         </div>
     </nav>
 
+    <!-- Sorting Menu -->
+    <div>
+        <form action="{{ route('search') }}" method="GET">
+        <select name="sort" onchange="this.form.submit()">
+            <option value="price_asc" {{ request('sort') == 'price_asc' ? 'selected' : '' }}>Price: Low to High</option>
+            <option value="price_desc" {{ request('sort') == 'price_desc' ? 'selected' : '' }}>Price: High to Low</option>
+            <option value="name_asc" {{ request('sort') == 'name_asc' ? 'selected' : '' }}>Name: A-Z</option>
+            <option value="name_desc" {{ request('sort') == 'name_desc' ? 'selected' : '' }}>Name: Z-A</option>
+        </select>
+        </form>
+    </div>
 
     <!-- PAGE CONTENT -->
     <div class="category-wrapper">
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,12 +1,23 @@
 @extends('layout.layout')
 
+<script>
+    var categories = @json($categories);
+
+    
+</script>   
+
+
+@foreach ($categories as $category)
+                                       
+<li><a href="{{route("home")}}?category_id={{$category->id}}">{{ $category->name }}</a></li>
+
+@endforeach
+
+{{--
 @section('page-content')
 @foreach ($categories as $category)
     <div>{{ $category->name }}</div>
     
 @endforeach
 @endsection
-
-<script>
-    var categories = @json($categories);
-</script>
+--}}

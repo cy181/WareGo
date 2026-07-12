b374439929ee31b8af99d347bac521093a9e5b5b
lugh <yanminwai122@gmail.com>
2026-07-13 01:09:07 +0900

    CSS clean up

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon" />
     <link href='{{ asset('bootstrap/css/bootstrap.min.css') }}' rel="stylesheet" />
-    <link rel="stylesheet" href='{{ asset('css/common.css') }}' />
+    <link rel="stylesheet" href='{{ asset('css/common.css') }}?v={{ time() }}' />
     @yield('custom-css')
     @yield('page-title')
 </head>
